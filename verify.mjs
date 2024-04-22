0bb0fa448644a1ff1da4416b40b21ae6ce18cf69
fbc <firatberkcakar@gmail.com>
2024-04-22 19:07:55 +0200

    refactored the verify script to do verification for both raffles

--- verify.mjs
+++ verify.mjs
@@ -1,27 +1,54 @@
 import { VRFHelper } from "./VRF-helper.mjs";
-import { getParticipantsFileHash, pickAllRaffleWinners,assignRangesToUsers } from "./utils.mjs";
+import { getParticipantsFileHash, pickAllRaffleWinners, assignRangesToUsers,pickAllRaffleWinnersForTheSecondRaffle } from "./utils.mjs";
 
 const contract = new VRFHelper(
     'https://api.avax.network/ext/bc/C/rpc',
-    null, // no need for a private key for read only
+    '', // Be sure to keep your private key secure!
     '0x847B953fE0D759011dF7Bc89d5700d757eC73Efa',
     './contractABI.json'
 );
-const ranges = assignRangesToUsers('./data/snapshot.json');
-const snapshotHash = getParticipantsFileHash('./data/snapshot.json');
-console.log(`Snapshot hash ${snapshotHash}`);
-const result = await contract.getResultForSnapshotHash(snapshotHash);
-console.log(`Random number : ${result}`);
-pickAllRaffleWinners(ranges,result,'winnersForVerification.json');
-const originalWinners = getParticipantsFileHash('winnersForVerification.json');
-const verifiedWinners = getParticipantsFileHash('winner.json');
-const verified = originalWinners === verifiedWinners;
-if(verified) {
-    console.log('Verification succesfull! The exact same result has been succesfully reconstructed!')
+
+async function verifyRaffle(snapshotFilePath, originalWinnersPath, verificationWinnersPath) {
+    const ranges = assignRangesToUsers(snapshotFilePath);
+    const snapshotHash = getParticipantsFileHash(snapshotFilePath);
+    console.log(`Snapshot hash ${snapshotHash}`);
+    const result = await contract.getResultForSnapshotHash(snapshotHash);
+    console.log(`Random number : ${result}`);
+    pickAllRaffleWinners(ranges, result, verificationWinnersPath);
+    const originalWinners = getParticipantsFileHash(verificationWinnersPath);
+    const verifiedWinners = getParticipantsFileHash(originalWinnersPath);
+    const verified = originalWinners === verifiedWinners;
+    if (verified) {
+        console.log('Verification succesfull! The exact same result has been succesfully reconstructed!')
+    }
+    else {
+        console.log('Verification failed!')
+    }
 }
-else {
-    console.log('Verification failed!')
+
+async function verifySecondRaffle(snapshotFilePath, originalWinnersPath, verificationWinnersPath) {
+    const ranges = assignRangesToUsers(snapshotFilePath);
+    const snapshotHash = getParticipantsFileHash(snapshotFilePath);
+    console.log(`Snapshot hash ${snapshotHash}`);
+    const result = await contract.getResultForSnapshotHash(snapshotHash);
+    console.log(`Random number : ${result}`);
+    pickAllRaffleWinnersForTheSecondRaffle(ranges, result, verificationWinnersPath);
+    const originalWinners = getParticipantsFileHash(verificationWinnersPath);
+    const verifiedWinners = getParticipantsFileHash(originalWinnersPath);
+    const verified = originalWinners === verifiedWinners;
+    if (verified) {
+        console.log('Verification succesfull! The exact same result has been succesfully reconstructed!')
+    }
+    else {
+        console.log('Verification failed!')
+    }
 }
+
+
+console.log('Verification for the first raffle: ');
+await verifyRaffle('./data/snapshot.json', 'winner.json', 'winnersForVerification.json');
+console.log('Verification for the second raffle: ');
+await verifySecondRaffle('./data/filtered_second_raffle_snapshot.json', 'secondRaffleWinner.json', 'winnersForVerificationRaffle2.json');
 
 
 
